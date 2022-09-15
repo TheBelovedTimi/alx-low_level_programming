c54c89c462b415c4466d9261b0f71bb6e0009012
TheBelovedTimi <tbtskyfree@gmail.com>
2022-09-15 15:25:18 -0700

    debugging tasks

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -10,6 +10,14 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+
+	/*
+	 * while (i < 10)
+	 * {
+	 * Infinte loop - No increase of variable
+	 * putchar(i);
+	 * }
+	 */
 	printf("infinite loop avoided!  \\o/\n");
 
 	return (0);
