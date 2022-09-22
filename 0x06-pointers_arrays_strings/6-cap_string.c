82b0d3798ab18d1261e57358bb38050d443300df
TheBelovedTimi <tbtskyfree@gmail.com>
2022-09-22 11:25:12 -0700

    Todays tasks

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -7,7 +7,7 @@
 char *cap_string(char *s)
 {
 	int count = 0, i;
-	int seperator[] >= {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	int separators[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
 
 	if (*(s + count) >= 97 && *(s + count) <= 122)
 		*(s + count) = *(s + count) - 32;
@@ -16,7 +16,7 @@ char *cap_string(char *s)
 	{
 		for (i = 0; i < 13; i++)
 		{
-			if (*(s + count) == seperator[i])
+			if (*(s + count) == separators[i])
 			{
 				if ((*(s + (count + 1)) >= 97) && (*(s + (count +1)) <= 122))
 					*(s + (count + 1)) = *(s (count + 1)) - 32;
