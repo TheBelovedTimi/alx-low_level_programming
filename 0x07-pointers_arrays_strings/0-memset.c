2e76dbc562ae29f75f89d685203c61dea90b49e8
TheBelovedTimi <tbtskyfree@gmail.com>
2022-09-26 10:00:43 -0700

    Todays Tasks

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,9 +11,9 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	/*Delacring FOR*/
-	for (i = 0; i < n; i++)
+	for (i = 0; n > 0; i++, n--)
 	{
-		*(s + i) = b; /*add 1 position s*/
+		s[i] = b;
 	} /*END FOR*/
 	return (s);
 }
