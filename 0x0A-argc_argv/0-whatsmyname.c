267f29490d015163fcc9b0ab4dad08028fcd815b
TheBelovedTimi <tbtskyfree@gmail.com>
2022-09-30 12:46:48 -0700

    My tasks

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+nclude <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: count argument
+ * @argv: argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - entry point
+ * @argc: count argument
+ * @argv: argument
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%i\n", argc - 1);
+	Return (0);
+}
